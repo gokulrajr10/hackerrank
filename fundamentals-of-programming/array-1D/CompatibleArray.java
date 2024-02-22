9e0342ef570a7e5d4d9eed0e5cb9a50f219e5fa7
Gokulraj R <rgokulraj1093@gmail.com>
2024-02-22 10:02:39 +0530

    commit

--- fundamentals-of-programming/array-1D/CompatibleArray.java
+++ fundamentals-of-programming/array-1D/CompatibleArray.java
@@ -1,33 +1,35 @@
 import java.util.*;
 
-public class SameOrNot {
+public class CompatibleArray {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
         int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
+        int c=0;
         int a1[] = new int[n1];
-        int a2[] = new int[n2];
         for(int i=0;i<n1;i++){
             a1[i] = sc.nextInt();
         }
+        int n2 = sc.nextInt();
+        int a2[] = new int[n2];
         for(int i=0;i<n2;i++){
             a2[i] = sc.nextInt();
         }
         if(n1==n2){
-            int s1=0,s2=0;
             for(int i=0;i<n1;i++){
-                s1 += a1[i];
-                s2 += a2[i];
+                if(a1[i]>=a2[i]){
+                    c=1;
+                }else{
+                    c=0;
+                    break;
+                }
             }
-            if(s1==s2){
-                System.out.println("Same");
-            }else{
-                System.out.println("Not Same");
-            }
+        }
+        if(c==1){
+            System.out.println("Compatible");
         }else{
-            System.out.println("Not Same");
+            System.out.println("Incompatible");
         }
         sc.close();
     }
