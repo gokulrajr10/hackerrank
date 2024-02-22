228d1ac9912f2889d27e76a42d6611b134dbaca7
Gokulraj R <rgokulraj1093@gmail.com>
2024-02-22 09:42:54 +0530

    initial commit

---
+++ fundamentals-of-programming/1D-Array/same-or-not.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int a1[] = new int[n1];
+        int a2[] = new int[n2];
+        for(int i=0;i<n1;i++){
+            a1[i] = sc.nextInt();
+        }
+        for(int i=0;i<n2;i++){
+            a2[i] = sc.nextInt();
+        }
+        if(n1==n2){
+            int s1=0,s2=0;
+            for(int i=0;i<n1;i++){
+                s1 += a1[i];
+                s2 += a2[i];
+            }
+            if(s1==s2){
+                System.out.println("Same");
+            }else{
+                System.out.println("Not Same");
+            }
+        }else{
+            System.out.println("Not Same");
+        }
+    }
+}
