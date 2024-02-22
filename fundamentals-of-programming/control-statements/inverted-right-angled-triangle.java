47815c01be5b26bde419d4f2cc71950bd61b3064
Gokulraj R <rgokulraj1093@gmail.com>
2024-02-22 09:54:33 +0530

    new branch

---
+++ fundamentals-of-programming/control-statements/inverted-right-angled-triangle.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k=n;
+        for(int i=0;i<n;i++){
+            for(int j=k;j!=0;j--){
+                System.out.print("*");
+            }
+            k--;
+            System.out.println("");
+        }
+    }
+}
